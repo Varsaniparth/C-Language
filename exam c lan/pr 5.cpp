448c2702e08ad0e1cd5d7757d623421ba140fe39
Varsaniparth <varsaniparth345@gmail.com>
2024-09-11 21:19:02 +0530

    Update pr 5.cpp

--- exam c lan/pr 5.cpp
+++ exam c lan/pr 5.cpp
@@ -3,14 +3,14 @@
 int main() {
     int i, j;
 
-    // Loop through each row
+    
     for (i = 0; i < 5; i++) {
-        // Print the starting numbers in decreasing order for each row
+        
         for (j = 10 - i; j <= 10; j++) {
             printf("%d", j);
         }
 
-        // Move to the next line after printing the row
+        
         printf("\n");
     }
 
